bb0dd565a73d9bf70a2a667f4f086eaaa29257e9
Ijier <ijier.dev@gmail.com>
2025-10-23 23:18:04 +0900

    feat: add sort method and output result

--- batch/WordCount/src/application/wordcount_application_service.py
+++ batch/WordCount/src/application/wordcount_application_service.py
@@ -1,6 +1,7 @@
-from typing import Tuple
+from typing import Iterable, Tuple
 
 import apache_beam as beam
+from apache_beam.pvalue import PCollection
 from apache_beam.runners.runner import PipelineResult
 from domain.wordcount_domain import (
     WordCount,
@@ -21,7 +22,7 @@ def build_word_count_pipeline(
     lines = pipeline | "ReadLines" >> beam.io.ReadFromText(input_path)
 
     # 2. Count words usig domain service
-    counts: Tuple[str, int] = (
+    counts: PCollection[Tuple[str, int]] = (
         lines
         # parse words using domain service
         | "SplitWords" >> (beam.ParDo(WordExtractorDoFn()).with_output_types(str))
@@ -31,11 +32,36 @@ def build_word_count_pipeline(
         | "GroupAndSum" >> beam.CombinePerKey(sum)
     )
 
+    # 2.1 prepare sort key
+    # countにマイナスをつけて、昇順ソートで降順を実現する。
+    sort_ready: PCollection[Tuple[int, str]] = counts | "InvertCount" >> beam.Map(
+        lambda item: (-item[1], item[0])
+    )
+
+    all_in_one_group: PCollection[Tuple[None, Iterable[Tuple[int, str]]]] = (
+        sort_ready
+        | "AddDummyKey" >> beam.Map(lambda item: (None, item))
+        | "GroupAllData" >> beam.GroupByKey()
+    )
+
+    sorted_count: PCollection[Tuple[int, str]] = (
+        all_in_one_group
+        | "SortAndFlatten" >> beam.FlatMap(lambda item: sorted(item[1]))
+    )
+
+    final_sorted_counts: PCollection[Tuple[str, int]] = (
+        sorted_count | "RestoreFormat" >> beam.Map(lambda item: (item[1], -item[0]))
+    )
+
     # 2.5 Convert to WordCount dataclass
-    typed_counts: WordCount = counts | "ToWordCount" >> beam.MapTuple(WordCount)
+    typed_counts: PCollection[WordCount] = (
+        final_sorted_counts | "ToWordCount" >> beam.MapTuple(WordCount)
+    )
 
     # 3. Format the result using domain service
-    output = typed_counts | "FormatResult" >> beam.Map(format_word_count_result)
+    output: PCollection[str] = typed_counts | "FormatResult" >> beam.Map(
+        format_word_count_result
+    )
 
     # 4. Write output text file
     output | "WriteResults" >> beam.io.WriteToText(output_path)
